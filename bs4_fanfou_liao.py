cca604da0127cdf2d8f7373833de25e9ace82d4c
Leejun Liao <liaoleejun@163.com>
2019-08-13 15:14:45 +0800

    添加"脚本运行时长"逻辑, 打算改成异步发送请求的形式

--- bs4_fanfou_liao.py
+++ bs4_fanfou_liao.py
@@ -6,6 +6,9 @@
 
 import requests
 from bs4 import BeautifulSoup
+import time
+
+start_time = time.time()
 
 login_url = 'http://fanfou.com/login'
 # payload是用Chrome监测登录的网络看到的, 其中用户名和密码是注册的
@@ -22,17 +25,17 @@ with requests.Session() as s:
     s.post(login_url, data=payload)
     res = '<meta http-equiv="Content-Type" content="text/html; ' \
           'charset=utf-8" />'
-    # 119是人工看到的网页总共有119页 (Todo 有待进一步优化, 来实现自动化)
+    # 人工看到的网页总共有118页 (Todo 有待进一步优化, 来实现自动化)
     for i in range(1, 119):
         page_url = 'http://fanfou.com/~RLhcIDBjZAM/p.' + str(i)
         content = s.get(page_url).text
         # 带上第二个参数 "html.parser", 否则会报 warning
         soup = BeautifulSoup(content, "html.parser")
         text = soup.find_all("span", class_="content")
-        time = soup.find_all("a", class_="time")
+        datetime = soup.find_all("a", class_="time")
 
         for j in range(len(text)):
-            res = res + str(time[j]) + ' ' + str(text[j]) + '<br><br>'
+            res = res + str(datetime[j]) + ' ' + str(text[j]) + '<br><br>'
         res += '<hr>'
         print(str(i) + 'th page')
 
@@ -41,3 +44,5 @@ with requests.Session() as s:
 fp = open("fanfou.html", "w", encoding="utf-8")
 fp.write(res)
 fp.close()
+
+print("--- %s seconds ---" % (time.time() - start_time))
